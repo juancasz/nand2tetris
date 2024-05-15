6b8de5ecfd40ebd4cae7d0b2641d60891d89f760
juan <juancasz0208@gmail.com>
2024-05-14 23:46:40 -0500

    Using all write commands

--- project8/vmtranslator/cmd/main.go
+++ project8/vmtranslator/cmd/main.go
@@ -39,12 +39,8 @@ func main() {
 }
 
 func write(parser *vmtranslator.Parser, codeWriter *vmtranslator.CodeWriter) error {
-	var commandType vmtranslator.CommandType
-	var arg1 string
-	var arg2 int
-	var err error
 	for {
-		err = parser.Advance()
+		err := parser.Advance()
 		if errors.Is(err, vmtranslator.ErrNoMoreCommands) {
 			break
 		}
@@ -52,39 +48,63 @@ func write(parser *vmtranslator.Parser, codeWriter *vmtranslator.CodeWriter) err
 			return err
 		}
 
-		commandType, err = parser.CommandType()
+		commandType, err := parser.CommandType()
 		if err != nil {
 			return err
 		}
 
-		arg1, err = parser.Arg1()
+		arg1, err := parser.Arg1()
 		if err != nil {
 			return err
 		}
 
 		switch commandType {
 		case vmtranslator.C_ARITHMETIC:
-			if err = codeWriter.WriteArithmetic(arg1); err != nil {
+			if err := codeWriter.WriteArithmetic(arg1); err != nil {
 				return err
 			}
 		case vmtranslator.C_POP:
-			arg2, err = parser.Arg2()
+			arg2, err := parser.Arg2()
 			if err != nil {
 				return err
 			}
-			if err = codeWriter.WritePushPop(vmtranslator.Pop, arg1, arg2); err != nil {
+			if err := codeWriter.WritePushPop(vmtranslator.Pop, arg1, arg2); err != nil {
 				return err
 			}
 		case vmtranslator.C_PUSH:
-			arg2, err = parser.Arg2()
+			arg2, err := parser.Arg2()
 			if err != nil {
 				return err
 			}
-			if err = codeWriter.WritePushPop(vmtranslator.Push, arg1, arg2); err != nil {
+			if err := codeWriter.WritePushPop(vmtranslator.Push, arg1, arg2); err != nil {
 				return err
 			}
 		case vmtranslator.C_COMMENT:
-			if err = codeWriter.WriteComment(arg1); err != nil {
+			if err := codeWriter.WriteComment(arg1); err != nil {
+				return err
+			}
+		case vmtranslator.C_LABEL:
+			if err := codeWriter.WriteLabel(arg1); err != nil {
+				return err
+			}
+		case vmtranslator.C_GOTO:
+			if err := codeWriter.WriteGoTo(arg1); err != nil {
+				return err
+			}
+		case vmtranslator.C_IF:
+			if err := codeWriter.WriteIf(arg1); err != nil {
+				return err
+			}
+		case vmtranslator.C_FUNCTION:
+			arg2, err := parser.Arg2()
+			if err != nil {
+				return err
+			}
+			if err := codeWriter.WriteFunction(arg1, arg2); err != nil {
+				return err
+			}
+		case vmtranslator.C_RETURN:
+			if err := codeWriter.WriteReturn(); err != nil {
 				return err
 			}
 		}
