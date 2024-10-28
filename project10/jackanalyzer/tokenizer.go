6792214cee552e21057d6eca6f2c1e2b80bc09c7
juan <juancasz0208@gmail.com>
2024-10-27 20:28:56 -0500

    feat: adding advance method and defining token types

--- project10/jackanalyzer/tokenizer.go
+++ project10/jackanalyzer/tokenizer.go
@@ -2,6 +2,7 @@ package jackanalyzer
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -13,9 +14,27 @@ const (
 	jack = ".jack"
 )
 
+var ErrNoMoreCommands = errors.New("no more commands")
+
 type Tockenizer struct {
-	fileScanner *bufio.Scanner
-	fileOS      *os.File
+	fileScanner  *bufio.Scanner
+	fileOS       *os.File
+	currentToken token
+}
+
+type TokenType int
+
+const (
+	KEYWORD TokenType = iota
+	SYMBOL
+	IDENTIFIER
+	INT_CONST
+	STRING_CONST
+)
+
+type token struct {
+	line      string
+	tokenType TokenType
 }
 
 func NewTockenizer(inputFile string) (*Tockenizer, error) {
@@ -37,8 +56,46 @@ func NewTockenizer(inputFile string) (*Tockenizer, error) {
 	}, nil
 }
 
-func (t *Tockenizer) hasMoreCommands() bool {
+func (t *Tockenizer) hasMoreTokens() bool {
 	return t.fileScanner.Scan()
+}
+
+func (t *Tockenizer) tokenType() (TokenType, error) {
+	return TokenType(0), nil
+}
+
+func (t *Tockenizer) Advance() error {
+	var line string
+	for {
+		if !t.hasMoreTokens() {
+			return ErrNoMoreCommands
+		}
+
+		line = t.fileScanner.Text()
+		if idx := strings.Index(line, "//"); idx != -1 {
+			line = line[:idx]
+		}
+
+		// Remove multi-line comments
+		if startIdx := strings.Index(line, "/*"); startIdx != -1 {
+			if endIdx := strings.Index(line[startIdx:], "*/"); endIdx != -1 {
+				line = line[:startIdx] + line[startIdx+endIdx+2:]
+			} else {
+				line = line[:startIdx] // If no closing, remove from startIdx to end
+			}
+		}
+
+		line = strings.TrimSpace(line)
+
+		if line == "" {
+			continue
+		}
+
+		break
+	}
+
+	t.currentToken.line = line
+	return nil
 }
 
 func (t *Tockenizer) Close() error {
